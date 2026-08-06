80acfb9154265824202a1b32221b9b978dd7044a
anwer1919 <98690064+anwer1919@users.noreply.github.com>
2026-08-06 22:43:52 +0300

    Create Home component with multiple subcomponents

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import NavBar from '../components/NavBar';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Companies from '../components/Companies';
+import Journey from '../components/Journey';
+import Media from '../components/Media';
+import Gallery from '../components/Gallery';
+import Contact from '../components/Contact';
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Companies />
+      <Journey />
+      <Media />
+      <Gallery />
+      <Contact />
+    </>
+  );
+}
