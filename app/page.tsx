4096d1b978c14de72524d42c5e49de0dbd236c07
anwer1919 <98690064+anwer1919@users.noreply.github.com>
2026-08-07 04:19:51 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
 import NavBar from '../components/NavBar';
 import Hero from '../components/Hero';
+import Projects from '../components/Projects';
 import About from '../components/About';
-import Companies from '../components/Companies';
-import Journey from '../components/Journey';
-import Media from '../components/Media';
-import Gallery from '../components/Gallery';
-import Contact from '../components/Contact';
+import Social from '../components/Social';
+import Footer from '../components/Footer';
 
 export default function Home() {
-  return (
-    <>
-      <NavBar />
-      <Hero />
-      <About />
-      <Companies />
-      <Journey />
-      <Media />
-      <Gallery />
-      <Contact />
-    </>
-  );
+  return (<>
+    <NavBar />
+    <Hero />
+    <Projects />
+    <About />
+    <Social />
+    <Footer />
+  </>);
 }
