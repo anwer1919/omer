32825745fde3d3f425342f3d2e4f00b91173856c
anwer1919 <98690064+anwer1919@users.noreply.github.com>
2026-08-09 02:01:12 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,7 @@ export default function Home() {
       <OaDivider />
       <Statement />
       <Work />
-      <OaDivider />
       <About />
-      <OaDivider />
       <Social />
       <Footer />
     </main>
